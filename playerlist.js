ce095f5ed4d2c2aa67aa9504fe3ad0ac841dcf7b
yuri1992 <yuri1992@gmail.com>
2014-09-13 10:25:01 +0300

    version 0.1

---
+++ playerlist.js
@@ -0,0 +1,111 @@
+var players = 
+			[
+				{
+					id:1,
+					name:'Ofek Pahima',
+					skill:05,
+					picture:'football.jpg'
+				},
+				{
+					id:2,
+					name:'Dan Bendror',
+					skill:65,
+					picture:'football.jpg'
+				},
+				{
+					id:3,
+					name:'Beni RedHair',
+					skill:55,
+					picture:'football.jpg'
+				},
+				{
+					id:4,
+					name:'Benzi Cohen',
+					skill:72,
+					picture:'football.jpg'
+				},
+				{
+					id:5,
+					name:'Gal Levi',
+					skill:85,
+					picture:'football.jpg'
+				},
+				{
+					id:6,
+					name:'Ran Kapitolnik',
+					skill:66,
+					picture:'football.jpg'
+				},
+				{
+					id:7,
+					name:'Tal Baril',
+					skill:70,
+					picture:'football.jpg'
+				},
+				{
+					id:8,
+					name:'Yaniv Gerbi',
+					skill:81,
+					picture:'football.jpg'
+				},
+				{
+					id:9,
+					name:'Kfir Ori',
+					skill:74,
+					picture:'football.jpg'
+				},
+				{
+					id:10,
+					name:'Saar Bergerbest',
+					skill:88,
+					picture:'football.jpg'
+				},
+				{
+					id:11,
+					name:'Omer Wienberg',
+					skill:66,
+					picture:'football.jpg'
+				},
+				{
+					id:12,
+					name:'Sagi Barzili',
+					skill:90,
+					picture:'football.jpg'
+				},
+				{
+					id:13,
+					name:'Shahaf Artzi',
+					skill:77,
+					picture:'football.jpg'
+				},
+				{
+					id:14,
+					name:'Tomer Mizrachi',
+					skill:60,
+					picture:'football.jpg'
+				},
+				{
+					id:15,
+					name:'Kinan Ben Shabat',
+					skill:70,
+					picture:'football.jpg'
+				},
+				{
+					id:16,
+					name:'Ofir Chen',
+					skill:82,
+					picture:'football.jpg'
+				},
+				{
+					id:17,
+					name:'Rafi Fadlon',
+					skill:52,
+					picture:'football.jpg'
+				},
+				{
+					id:18,
+					name:'Yuri Ritvin',
+					skill:80,
+					picture:'football.jpg'
+				}
+			];
