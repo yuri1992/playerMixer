c34c73f350c55f506004a25efdbcfb60a284eb04
Yuri Ritvin <yuri-r@yit.co.il>
2014-09-17 17:39:10 +0300

    update scores

--- playerlist.js
+++ playerlist.js
@@ -3,109 +3,109 @@ var players =
 				{
 					id:1,
 					name:'Ofek Pahima',
-					skill:05,
+					skill:78,
 					picture:'ofekpahima.jpg'
 				},
 				{
 					id:2,
 					name:'Dan Bendror',
-					skill:65,
+					skill:54,
 					picture:'dandeb.jpg'
 				},
 				{
 					id:3,
 					name:'Beni RedHair',
-					skill:55,
+					skill:58,
 					picture:'benibarki.jpg'
 				},
 				{
 					id:4,
 					name:'Benzi Cohen',
-					skill:72,
+					skill:39,
 					picture:'bencohen.jpg'
 				},
 				{
 					id:5,
 					name:'Gal Levi',
-					skill:85,
+					skill:75,
 					picture:'gallevi.jpg'
 				},
 				{
 					id:6,
 					name:'Ran Kapitolnik',
-					skill:66,
+					skill:6,
 					picture:'ranka.jpg'
 				},
 				{
 					id:7,
 					name:'Tal Baril',
-					skill:70,
+					skill:16,
 					picture:'talbaril.jpg'
 				},
 				{
 					id:8,
 					name:'Yaniv Gerbi',
-					skill:81,
+					skill:55,
 					picture:'yanivgerbi.jpg'
 				},
 				{
 					id:9,
 					name:'Kfir Ori',
-					skill:74,
+					skill:50,
 					picture:'orikfir.jpg'
 				},
 				{
 					id:10,
 					name:'Saar Bergerbest',
-					skill:88,
+					skill:70,
 					picture:'saarber.jpg'
 				},
 				{
 					id:11,
 					name:'Omer Wienberg',
-					skill:66,
+					skill:29,
 					picture:'omerwien.jpg'
 				},
 				{
 					id:12,
 					name:'Sagi Barzili',
-					skill:90,
+					skill:78,
 					picture:'sagibar.jpg'
 				},
 				{
 					id:13,
 					name:'Shahaf Artzi',
-					skill:77,
+					skill:40,
 					picture:'artzi.jpg'
 				},
 				{
 					id:14,
 					name:'Tomer Mizrachi',
-					skill:60,
+					skill:11,
 					picture:'tomermizrachi.jpg'
 				},
 				{
 					id:15,
 					name:'Kinan Ben Shabat',
-					skill:70,
+					skill:7,
 					picture:'keynanbenshabat.jpg'
 				},
 				{
 					id:16,
 					name:'Ofir Chen',
-					skill:82,
+					skill:70,
 					picture:'ofirchen.jpg'
 				},
 				{
 					id:17,
 					name:'Rafi Fadlon',
-					skill:52,
+					skill:5,
 					picture:'rafifadlon.jpg'
 				},
 				{
 					id:18,
 					name:'Yuri Ritvin',
-					skill:80,
+					skill:77,
 					picture:'yuriritvin.jpg'
 				}
 			];
